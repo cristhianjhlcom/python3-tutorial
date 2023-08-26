ae3e3a59604c43fcec0346ade5443d6ee1f8b779
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-08-25 20:51:34 -0500

    add new basics exercises

---
+++ basics/exercises/ground-shipping.py
@@ -0,0 +1,25 @@
+# Envíos por Tierra 🚚
+
+# Nos pidieron crear un programa que calcule el precio de un envío terrestre.
+# El precio total va a depender del peso del paquete.
+
+# Vas a utilizar tus conocimientos en:
+# Variables & Asignación.
+# Estructuras de control selectivas.
+# Operadores aritmeticos.
+
+# Puntos realizar
+
+# Variable para el "peso". Puede ser el valor que quieras por ejemplo 8.8
+# Variable para almacenar el costo total.
+
+# Si el "peso" es menor o igual a 2.
+#   Entonces asigna al costo total "peso" multiplicado por 1.50 más 20
+# Si el "peso" es mayor a 2 y si el "peso" es igual a 6.
+#   Entonces asigna al costo total "peso" multiplicado por 3.00 más 20
+# Si el "peso" es mayor a 6 o el "peso" es igual a 10.
+#   Entonces asigna al costo total "peso" multiplicado por 4.00 más 20
+# Si ninguno se cumple.
+#   Entonces asigna al costo total "peso" multiplicado por 4.75 más 20
+
+# Imprime el costo total con el siguiente formato "El costo total a pagar es de $[total_cost]"
---
+++ basics/exercises/magic-ball.py
@@ -0,0 +1,32 @@
+# Magic Ball Game 🎱
+
+# Crea las siguientes variables 📦
+# Asigna el valor que corresponde.
+
+# Tu nombre.
+# Pregunta que quieres hacer.
+# Respuesta. (Iniciar como un string vacio)
+# Número aleatorio.
+
+# Para generar un número aleatorio del 1 al 9 puedes usar el modulo random de python.
+# Puedes encontrar más información aquí.
+# https://www.w3schools.com/python/ref_random_randint.asp
+
+# Para este paso debe usar tus conocimientos en estructuras de control.
+# Dependiendo el valor almacenado en la variable "respuesta".
+# Va a depender del número generado aleatoriamente.
+
+# Si el número es 1 almacena "Si - Definitivamente"
+# Si el número es 2 almacena "Es decididamente así"
+# Si el número es 3 almacena "Sin ninguna duda"
+# Si el número es 4 almacena "Respuesta confusa, intenta otra vez"
+# Si el número es 5 almacena "Pregunta de nuevo más tarde"
+# Si el número es 6 almacena "Mejor no decirte ahora"
+# Si el número es 7 almacena "Mis fuentes dicen que no"
+# Si el número es 8 almacena "No es una vista muy prometedora"
+# Si el número es 9 almacena "Muy cuestionable"
+# Si no es ninguno almacena "Error"
+
+# Imprime algo con este formato "[nombre] asks: [pregunta]"
+# Imprime algo con este formato "La bola mágica 8 🎱 responde: [respuesta]"
+
