548ea0225b5634c6f011be2b9b45f93a2640e738
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2023-09-27 23:01:11 -0500

    add new easy exercise

---
+++ exercises/team-name-generator.py
@@ -0,0 +1,13 @@
+# Generador del nombre de tu equipo.
+
+# TODOS
+# 1. Crea e imprime un soludo para el programa.
+# 2. Preguntale al usuario la ciudad donde nacio.
+# 3. Preguntale el nombre de su animal favorito.
+# 4. Combina las variables e imprime un mensaje e.g.:
+# "Los {animal} de {city}"
+
+# Tips
+# Usar el builtin function de python 'input'
+# La respuesta del usuario almacenalo en variables.
+# Utiliza los formatos de strings para imprimir tu mensaje.
