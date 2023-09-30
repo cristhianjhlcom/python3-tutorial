1c45d84b8bd66e492504f2a1b1abcbc7f488ac10
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-09-30 17:38:50 -0500

    add odd even sum exercise

---
+++ exercises/08-odd-even-sum.py
@@ -0,0 +1,13 @@
+## Suma de los pares y Suma de los impares
+# Dado un conjunto de números enteros,
+# se te pide que escribas un programa que calcule la suma de los números pares
+# y la suma de los números impares en ese conjunto por separado.
+## TODOS
+# 1. Crear una lista (o arreglo) para almacenar los números enteros.
+# 2. Inicializar dos variables, una para la suma de números pares y
+#   otra para la suma de números impares, ambas inicialmente en cero.
+# 3. Iterar a través de la lista de números.
+# 4. En cada iteración, verifica si el número es par o impar.
+# 5. Si es par, suma el número a la variable de suma de números pares.
+# 6. Si es impar, suma el número a la variable de suma de números impares.
+# 7. Al final, imprime las dos sumas.
