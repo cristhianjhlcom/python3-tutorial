8e73038ac66baf17b2c63f0ae24e427ac3055f51
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-09-30 17:38:38 -0500

    add word count exercise

---
+++ exercises/09-word-count.py
@@ -0,0 +1,12 @@
+## Contador de Palabras
+# Desarrolla un programa que cuente cuántas palabras
+# hay en una cadena de texto ingresada por el usuario.
+# Debes considerar que las palabras están separadas por espacios.
+## TODOS
+# 1. Solicitar al usuario que ingrese una cadena de texto.
+# 2. Inicializar una variable para llevar un registro del número de palabras, comenzando en 0.
+# 3. Iterar a través de cada carácter en la cadena.
+# 4. Para cada carácter, verificar si es un espacio en blanco (espacio, tabulación o salto de línea).
+# 5. Si encontramos un espacio en blanco, incrementar el contador de palabras en 1.
+# 6. Continuar el proceso hasta que hayamos recorrido toda la cadena.
+# 7. Al final, mostrar el número de palabras.
